6aa96ef082d6c5a6b81df48a94b7ea89e8c54336
gaylor-bussy <gaylor.bussy@gmail.com>
2026-04-21 09:43:46 +0200

    mediaplayerbase

---
+++ javascript.js
@@ -0,0 +1,61 @@
+   let chemin = document.getElementById("play");
+   let audio = document.getElementById("piste");
+   
+   let  lecture = false
+    chemin.addEventListener("click" , changeIconePlay);
+function changeIconePlay(){
+if (lecture == true ) {
+   let valeur=parseInt(audio.value, 0.1);
+    valeur = Math.min (100, valeur + 0.1);
+    audio.value=valeur;
+
+
+ chemin.innerHTML='<i class="fa-solid fa-play"></i>'
+ lecture=false;
+}
+else {
+   
+ chemin.innerHTML='<i class="fa-solid fa-pause"></i>'
+ lecture=true;
+}
+
+}
+
+
+
+let slider = document.getElementById("vrange");
+ 
+  const plus = document.getElementById("plus")
+ const moins = document.getElementById("moins")
+
+
+
+
+plus.addEventListener("click" , () => {
+ let valeur=parseInt(slider.value, 10);
+ valeur = Math.min (100, valeur + 10);
+    console.log(slider.value);
+
+ 
+ slider.value=valeur;
+ 
+});
+moins.addEventListener("click" , () => {
+ let valeur=parseInt(slider.value, 10);
+ valeur = Math.max (0, valeur - 10);
+    console.log(slider.value);
+
+ 
+ slider.value=valeur;
+ 
+});
+
+  
+
+
+  
+     /*let defaultVal = x.defaultValue;
+     let currentVal = x.value;
+
+     if (slider <= 100)*/
+       
